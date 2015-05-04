cd4c9b729bf6b71087541f1e2b80a3da2d79d88b
John Welch <jwelch@welchitconsulting.co.uk>
2015-05-04 11:33:13 +0100

    Initial code import

---
+++ db/access.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * Copyright (C) 2015 Welch IT Consulting
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Filename : access,php
+ * Author   : John Welch <jwelch@welchitconsulting.co.uk>
+ * Created  : 26 Apr 2015
+ */
+
+$capabilities = array(
+
+    'block/sbresources:addinstance' => array(
+
+        'riskbitmask'          => RISK_SPAM | RISK_XSS,
+        'captype'              => 'write',
+        'contextlevel'         => CONTEXT_BLOCK,
+        'archetypes'           => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager'        => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    )
+
+);
